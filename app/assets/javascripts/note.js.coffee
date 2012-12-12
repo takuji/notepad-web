56578d70a96d696e53a8dc5cdcfeab7d372464cc
Takuji Shimokawa <takuji.shimokawa@gmail.com>
2012-12-12 11:35:58 +0900

    textareaフォーカス時に縁が光るのを消す。

---
+++ app/assets/javascripts/note.js.coffee
@@ -0,0 +1,2 @@
+$ ->
+  $("body:has(.note)").css("backgroundColor": "#e4e4e4")
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121210152339) do
+ActiveRecord::Schema.define(:version => 20121211161730) do
 
   create_table "notes", :force => true do |t|
     t.integer  "user_id"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(:version => 20121210152339) do
     t.datetime "updated_at",                             :null => false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
