a6f8be700083b2ce4bf06aea145635a7029e4ed4
Takuji Shimokawa <takuji.shimokawa@gmail.com>
2013-08-13 18:59:38 +0900

    Prevents loading all data for the note list page.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,15 @@
 class Note < ActiveRecord::Base
   belongs_to :user
 
-  def title
+  before_save :update_title
+  
+  private
+  
+  def update_title
+    self.title = extract_title
+  end
+
+  def extract_title
     content ? content.split("\n", 2)[0] : "Untitiled"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130813070457) do
+ActiveRecord::Schema.define(version: 20130813094624) do
 
   create_table "notes", force: true do |t|
     t.integer  "user_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20130813070457) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.text     "html_content"
+    t.string   "title"
   end
 
   add_index "notes", ["user_id"], name: "index_notes_on_user_id", using: :btree
