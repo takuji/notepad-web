9346e6c5cf457ab0e5cdc8c7c1aa1fef405594dd
Takuji Shimokawa <takuji.shimokawa@gmail.com>
2013-09-13 01:10:31 +0900

    Add user icon.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130906171702) do
+ActiveRecord::Schema.define(version: 20130912153714) do
 
   create_table "evernote_accounts", force: true do |t|
     t.integer  "user_id"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20130906171702) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "use_evernote",           default: false
+    t.string   "image"
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
