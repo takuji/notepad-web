6050ef2d0f79bfa158761943c470a5f56b9c8591
Takuji Shimokawa <takuji.shimokawa@gmail.com>
2013-10-06 17:26:34 +0900

    Add note list to the dashboard page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
+      @notes = current_user.latest_notes.page(params[:page])
       render :dashboard
     end
   end
